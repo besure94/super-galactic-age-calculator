3c141cc954079064b148f6c70f557872006dca76
Brian Scherner <brianscherner@gmail.com>
2023-08-08 13:22:10 -0700

    Write method to calculate number of solar years that have passed since the users last birthday until their current age for each planet.

--- src/js/solaryears.js
+++ src/js/solaryears.js
@@ -2,6 +2,7 @@ export class SolarYears {
   constructor() {
     this.mercuryYears = 0;
     this.venusYears = 0;
+    this.earthYears = 0;
     this.marsYears = 0;
     this.jupiterYears = 0;
   }
@@ -13,7 +14,11 @@ export class SolarYears {
     this.jupiterYears = parseFloat((userAge / 11.86).toFixed(2));
   }
 
-  howManyYearsSincePastBday(userAge, pastBday) {
-
+  howManyYearsSincePastBday(userAge, pastBdayAge) {
+    this.earthYears = parseInt((userAge - pastBdayAge));
+    this.mercuryYears = parseFloat(((userAge - pastBdayAge) / 0.24).toFixed(2));
+    this.venusYears = parseFloat(((userAge - pastBdayAge) / 0.62).toFixed(2));
+    this.marsYears = parseFloat(((userAge - pastBdayAge) / 1.88).toFixed(2));
+    this.jupiterYears = parseFloat(((userAge - pastBdayAge) / 11.86).toFixed(2));
   }
 }
