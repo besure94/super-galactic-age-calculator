3132a045fa2432570488ed8592e95ff6f2be3650
Brian Scherner <brianscherner@gmail.com>
2023-08-08 11:44:58 -0700

    Update method calculating solar years for each planet.

--- src/js/solaryears.js
+++ src/js/solaryears.js
@@ -7,6 +7,9 @@ export class SolarYears {
   }
 
   calculateSolarYears(userAge) {
-
+    this.mercuryYears = parseFloat((userAge / .24).toFixed(2));
+    this.venusYears = parseFloat((userAge / .62).toFixed(2));
+    this.marsYears = parseFloat((userAge / 1.88).toFixed(2));
+    this.jupiterYears = parseFloat((userAge / 11.86).toFixed(2));
   }
 }
