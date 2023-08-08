549a95bf379c54da5c77deec8680a6e60ea179d7
Brian Scherner <brianscherner@gmail.com>
2023-08-08 14:06:55 -0700

    Edit all methods to include earthYears property.

--- src/js/solaryears.js
+++ src/js/solaryears.js
@@ -10,22 +10,23 @@ export class SolarYears {
   calculateSolarYears(userAge) {
     this.mercuryYears = parseFloat((userAge / 0.24).toFixed(2));
     this.venusYears = parseFloat((userAge / 0.62).toFixed(2));
+    this.earthYears = parseInt(userAge);
     this.marsYears = parseFloat((userAge / 1.88).toFixed(2));
     this.jupiterYears = parseFloat((userAge / 11.86).toFixed(2));
   }
 
   howManyYearsSincePastBday(userAge, pastBdayAge) {
-    this.earthYears = parseInt((userAge - pastBdayAge));
     this.mercuryYears = parseFloat(((userAge - pastBdayAge) / 0.24).toFixed(2));
     this.venusYears = parseFloat(((userAge - pastBdayAge) / 0.62).toFixed(2));
+    this.earthYears = parseInt((userAge - pastBdayAge));
     this.marsYears = parseFloat(((userAge - pastBdayAge) / 1.88).toFixed(2));
     this.jupiterYears = parseFloat(((userAge - pastBdayAge) / 11.86).toFixed(2));
   }
 
   howManyYearsUntilFutureBday(futureBdayAge, userAge) {
-    this.earthYears = parseInt((futureBdayAge - userAge));
     this.mercuryYears = parseFloat(((futureBdayAge - userAge) / 0.24).toFixed(2));
     this.venusYears = parseFloat(((futureBdayAge - userAge) / 0.62).toFixed(2));
+    this.earthYears = parseInt((futureBdayAge - userAge));
     this.marsYears = parseFloat(((futureBdayAge - userAge) / 1.88).toFixed(2));
     this.jupiterYears = parseFloat(((futureBdayAge - userAge) / 11.86).toFixed(2));
   }
