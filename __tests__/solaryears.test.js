dd89809d74cce491aa92d61dba4b76a1b6d9953d
Brian Scherner <brianscherner@gmail.com>
2023-08-08 13:45:03 -0700

    Add earthYears property to first and third test. Write failing test for method to determine how many years have yet to pass until the users future birthday for each planet.

--- __tests__/solaryears.test.js
+++ __tests__/solaryears.test.js
@@ -7,9 +7,10 @@ describe('SolarYears', () => {
     solarYears = new SolarYears();
   });
 
-  test('should create a Solar Years object with four properties, with each property being a 0', () => {
+  test('should create a Solar Years object with five properties, with each property being a 0', () => {
     expect(solarYears.mercuryYears).toEqual(0);
     expect(solarYears.venusYears).toEqual(0);
+    expect(solarYears.earthYears).toEqual(0);
     expect(solarYears.marsYears).toEqual(0);
     expect(solarYears.jupiterYears).toEqual(0);
   });
@@ -20,12 +21,20 @@ describe('SolarYears', () => {
     expect(solarYears.marsYears).toEqual(21.28);
     expect(solarYears.jupiterYears).toEqual(3.37);
   });
-  test('should contain a method that can determine how many years have passed on each planet since the users past birthday', () => {
+  test('should contain a method that can determine how many years have passed since the users past birthday, for each planet', () => {
     solarYears.howManyYearsSincePastBday(40, 35);
     expect(solarYears.earthYears).toEqual(5);
     expect(solarYears.mercuryYears).toEqual(20.83);
     expect(solarYears.venusYears).toEqual(8.06);
     expect(solarYears.marsYears).toEqual(2.66);
     expect(solarYears.jupiterYears).toEqual(0.42);
+  });
+  test('should contain a method that can determine how many years have yet to pass until the users future birthday, for each planet', () => {
+    solarYears.howManyYearsUntilFutureBday(40, 50);
+    expect(solarYears.earthYears).toEqual(10);
+    expect(solarYears.mercuryYears).toEqual(41.67);
+    expect(solarYears.venusYears).toEqual(16.13);
+    expect(solarYears.marsYears).toEqual(5.32);
+    expect(solarYears.jupiterYears).toEqual(0.84);
   });
 });
