0262634db5c8b177b8168b7d305906d26cf2b3b7
Brian Scherner <brianscherner@gmail.com>
2023-08-08 14:06:06 -0700

    Edit placement of expect statements for earthYears. Add expect statement for earthYears in second test.

--- __tests__/solaryears.test.js
+++ __tests__/solaryears.test.js
@@ -18,22 +18,23 @@ describe('SolarYears', () => {
     solarYears.calculateSolarYears(40);
     expect(solarYears.mercuryYears).toEqual(166.67);
     expect(solarYears.venusYears).toEqual(64.52);
+    expect(solarYears.earthYears).toEqual(40);
     expect(solarYears.marsYears).toEqual(21.28);
     expect(solarYears.jupiterYears).toEqual(3.37);
   });
   test('should contain a method that can determine how many years have passed since the users past birthday, for each planet', () => {
     solarYears.howManyYearsSincePastBday(40, 35);
-    expect(solarYears.earthYears).toEqual(5);
     expect(solarYears.mercuryYears).toEqual(20.83);
     expect(solarYears.venusYears).toEqual(8.06);
+    expect(solarYears.earthYears).toEqual(5);
     expect(solarYears.marsYears).toEqual(2.66);
     expect(solarYears.jupiterYears).toEqual(0.42);
   });
   test('should contain a method that can determine how many years have yet to pass until the users future birthday, for each planet', () => {
     solarYears.howManyYearsUntilFutureBday(50, 40);
-    expect(solarYears.earthYears).toEqual(10);
     expect(solarYears.mercuryYears).toEqual(41.67);
     expect(solarYears.venusYears).toEqual(16.13);
+    expect(solarYears.earthYears).toEqual(10);
     expect(solarYears.marsYears).toEqual(5.32);
     expect(solarYears.jupiterYears).toEqual(0.84);
   });
