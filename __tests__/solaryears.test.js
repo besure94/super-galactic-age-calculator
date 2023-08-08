561b09f5a88514cd100aff568eacbc6425dcf078
Brian Scherner <brianscherner@gmail.com>
2023-08-08 11:03:56 -0700

    Edit failing test wording.

--- __tests__/solaryears.test.js
+++ __tests__/solaryears.test.js
@@ -13,7 +13,7 @@ describe('SolarYears', () => {
     expect(solarYears.marsYears).toEqual(0);
     expect(solarYears.jupiterYears).toEqual(0);
   });
-  test('should contain a method that takes in a users age and returns their age in solar years for each of the four planets', () => {
+  test('should contain a method that takes in a users age and returns their age in solar years for each of the four above planets', () => {
     solarYears.calculateSolarYears(40);
     expect(solarYears.mercuryYears).toEqual(166.67);
     expect(solarYears.venusYears).toEqual(64.52);
--- src/js/solaryears.js
+++ src/js/solaryears.js
@@ -5,4 +5,8 @@ export class SolarYears {
     this.marsYears = 0;
     this.jupiterYears = 0;
   }
+
+  calculateSolarYears(userAge) {
+
+  }
 }
