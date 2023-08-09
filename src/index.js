05f75f9cefaa27042eb5e69a754f10434bc1e1c5
Brian Scherner <brianscherner@gmail.com>
2023-08-09 10:09:34 -0700

    Edit variable names in eventHandler and add querySelectors at end.

--- src/index.js
+++ src/index.js
@@ -8,19 +8,22 @@ function handleSolarYearsForm(event) {
   document.querySelector('#ageCalculations').innerText = null;
   document.querySelector('#pastBdayCalculations').innerText = null;
   document.querySelector('#futureBdayCalculations').innerText = null;
-  const userAge = parseInt(document.querySelector('#userAge').value);
-  const pastBdayAge = parseInt(document.querySelector('#pastBdayAge').value);
-  const futureBdayAge = parseInt(document.querySelector('#futureBdayAge').value);
+  const ageOfUser = parseInt(document.querySelector('#userAge').value);
+  const pastBirthdayAge = parseInt(document.querySelector('#pastBdayAge').value);
+  const futureBirthdayAge = parseInt(document.querySelector('#futureBdayAge').value);
   const solarYears = new SolarYears();
-  const ageInSolarYears = solarYears.calculateSolarYears(userAge);
-  const solarYearsSinceLastBday = solarYears.howManyYearsSincePastBday(pastBdayAge, futureBdayAge);
-  const solarYearsUntilFutureBday = solarYears.howManyYearsUntilFutureBday(futureBdayAge, userAge);
+  const ageInSolarYears = solarYears.calculateSolarYears(ageOfUser);
+  const solarYearsSinceLastBday = solarYears.howManyYearsSincePastBday(ageOfUser, pastBirthdayAge);
+  const solarYearsUntilFutureBday = solarYears.howManyYearsUntilFutureBday(futureBirthdayAge, ageOfUser);
   const pTagOne = document.createElement("p");
   const pTagTwo = document.createElement("p");
   const pTagThree = document.createElement("p");
   pTagOne.append(`Your age in solar years is: ${ageInSolarYears}.`);
   pTagTwo.append(`The number of solar years since your past birthday and current age is: ${solarYearsSinceLastBday}.`);
   pTagThree.append(`The number of solar years from your current age to your future birthday is: ${solarYearsUntilFutureBday}.`);
+  document.querySelector('#ageCalculations').append(pTagOne);
+  document.querySelector('#pastBdayCalculations').append(pTagTwo);
+  document.querySelector('#futureBdayCalculations').append(pTagThree);
 }
 
 window.addEventListener("load", function() {
