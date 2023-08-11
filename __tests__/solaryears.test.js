787c8869a0cca05e69010e9d9df8db20a962061a
Brian Scherner <brianscherner@gmail.com>
2023-08-11 13:00:28 -0700

    Edit test description, remove userAge argument from method, and add a new property for each planet storing the result of the formula.

--- __tests__/solaryears.test.js
+++ __tests__/solaryears.test.js
@@ -19,7 +19,7 @@ describe('SolarYears', () => {
     expect(solarYears.marsRate).toEqual(1.88);
     expect(solarYears.jupiterRate).toEqual(11.86);
   });
-  test('should contain a method that divides the userAge property by each one of the planet rate properties, and returns their age in solar years for each of the four planets', () => {
+  test('should contain a method that returns the users age in solar years for each of the four planets', () => {
     solarYears.calculateSolarYears();
     expect(solarYears.mercuryYears).toEqual(166.67);
     expect(solarYears.venusYears).toEqual(64.52);
@@ -27,14 +27,14 @@ describe('SolarYears', () => {
     expect(solarYears.marsYears).toEqual(21.28);
     expect(solarYears.jupiterYears).toEqual(3.37);
   });
-  // test('should contain a method that can determine how many years have passed since the users past birthday, for each planet', () => {
-  //   solarYears.howManyYearsSincePastBday(40, 35);
-  //   expect(solarYears.mercuryYears).toEqual(20.83);
-  //   expect(solarYears.venusYears).toEqual(8.06);
-  //   expect(solarYears.earthYears).toEqual(5);
-  //   expect(solarYears.marsYears).toEqual(2.66);
-  //   expect(solarYears.jupiterYears).toEqual(0.42);
-  // });
+  test('should contain a method that can determine how many years have passed since the users past birthday for each planet, and stores the result in a new property for each planet', () => {
+    solarYears.howManyYearsSincePastBday(35);
+    expect(solarYears.mercuryYearsPast).toEqual(20.83);
+    expect(solarYears.venusYearsPast).toEqual(8.06);
+    expect(solarYears.earthYearsPast).toEqual(5);
+    expect(solarYears.marsYearsPast).toEqual(2.66);
+    expect(solarYears.jupiterYearsPast).toEqual(0.42);
+  });
   // test('should contain a method that can determine how many years have yet to pass until the users future birthday, for each planet', () => {
   //   solarYears.howManyYearsUntilFutureBday(50, 40);
   //   expect(solarYears.mercuryYears).toEqual(41.67);
